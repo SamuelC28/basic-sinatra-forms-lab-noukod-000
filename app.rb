0d7a84489366e04f999a31c6f92330232c5ff21d
Samuel Cornet <samuel@noukod.com>
2019-01-07 20:35:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-     @the_team = [<%= "Team Name: #{@params[:name]}" %><br>
-                  <%= "Coach: #{@params[:coach]}" %><br>
-                  <%= "Point Guard: #{@params[:pg]}" %><br>
-                  <%= "Shooting Guard: #{@params[:sg]}" %><br>
-                  <%= "Small Forward: #{@params[:sf]}" %><br>
-                  <%= "Power Forward: #{@params[:pf]}" %><br>
-                  <%= "Center: #{@params[:c]}" %><br>]
+     @the_team = ['<%= "Team Name: #{@params[:name]}" %><br>'
+                  '<%= "Coach: #{@params[:coach]}" %><br>'
+                  '<%= "Point Guard: #{@params[:pg]}" %><br>'
+                  '<%= "Shooting Guard: #{@params[:sg]}" %><br>'
+                  '<%= "Small Forward: #{@params[:sf]}" %><br>'
+                  '<%= "Power Forward: #{@params[:pf]}" %><br>'
+                  '<%= "Center: #{@params[:c]}" %><br>']
     erb :team
   end
 end
